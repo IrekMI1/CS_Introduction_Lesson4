45853c569fc4def36d423e5702e175032f9cd89d
Irek <general157@mail.ru>
2022-08-30 22:34:02 +0300

    add Example25

---
+++ Example25/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+void  GetPowerInCycle()
+{
+    while (true)
+    {
+        Console.Write("Введите целое основание:  \t");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите целую степень:  \t");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Результат: ({a})^({b}) = {Math.Pow(a, b)}");
+    }
+}
+
+try
+{
+    GetPowerInCycle();
+}
+catch 
+{
+    Console.WriteLine("Неверный ввод!");
+}
