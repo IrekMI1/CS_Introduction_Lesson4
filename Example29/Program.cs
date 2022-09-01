829fe4b7bf996b8eee17275708d837ddbd4ceea7
Irek <general157@mail.ru>
2022-09-01 19:46:26 +0300

    add Example29

---
+++ Example29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+
+int[] FillArrayFromInput()
+{
+    return Console.ReadLine().Split(", ").Select(x => Convert.ToInt32(x)).ToArray();
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}]");
+    }
+}
+
+try
+{
+    Console.Write("Введите числа через ', ' (a, b, c): \t");
+    int[] array = FillArrayFromInput(); 
+    PrintArray(array);
+}
+catch 
+{
+    Console.WriteLine("Неверный формат ввода!");
+}
